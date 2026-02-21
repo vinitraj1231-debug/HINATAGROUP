81e3b5c3aac79b4c5a89c95b8bed8177af1cce91
vinitraj1231-debug <vinitraj1231@gmail.com>
2026-02-22 01:01:33 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,8 @@
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
+OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
+SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "@YourSupportChat")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "0"))  # Optional log channel ID
+DATABASE_URL = os.getenv("DATABASE_URL", "nomade.db")
 
