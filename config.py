511fa2237f90cd56ff32a7a3cbc4554be2f8e6c6
vinitraj1231-debug <vinitraj1231@gmail.com>
2026-02-22 01:14:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-OWNER_ID = int(os.getenv("OWNER_ID", "123456789"))
-SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "@YourSupportChat")
-LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "0"))  # Optional log channel ID
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8564523423:AAE8FMTtmRfae7R-hHLYC06F5eeVJIJACYs")
+OWNER_ID = int(os.getenv("OWNER_ID", "8373641692"))
+SUPPORT_CHAT = os.getenv("SUPPORT_CHAT", "@narzofamily")
+LOG_CHANNEL = int(os.getenv("LOG_CHANNEL", "-1003730763488"))  # Optional log channel ID
 DATABASE_URL = os.getenv("DATABASE_URL", "nomade.db")
 
